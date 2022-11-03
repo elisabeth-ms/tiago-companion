c41b7310e879fc183f6a6a86bf037360137ac917
elisabeth-ms <emenende@pa.uc3m.es>
2022-11-03 19:06:27 +0100

    Changes in grasp_objects_node.

--- grasp_objects/include/grasp_objects/grasp_objects.hpp
+++ grasp_objects/include/grasp_objects/grasp_objects.hpp
@@ -1,4 +1,9 @@
 #include <ros/ros.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <tf/transform_listener.h>
+#include <pcl_ros/transforms.h>
+#include <pcl/filters/voxel_grid.h>
+
 namespace grasp_objects{
 class GraspObjects{
     public:
@@ -7,9 +12,19 @@ class GraspObjects{
 
     ~GraspObjects();
 
+    void init();
+
+    void pointCloudCallback(const sensor_msgs::PointCloud2ConstPtr &pointCloud_msg);
+
     private:
     //! ROS node handle.
     ros::NodeHandle nodeHandle_;
+    ros::Subscriber pointCloudSubscriber_;
+    ros::Publisher outPointCloudPublisher_;
+    tf::TransformListener listener_;
+    tf::StampedTransform transform_;
+    float epsAnglePlaneSegmentation_;
+    float distanceThresholdPlaneSegmentation_;
 
 };
 }
--- grasp_objects/src/grasp_objects.cpp
+++ grasp_objects/src/grasp_objects.cpp
@@ -3,10 +3,34 @@
 namespace grasp_objects{
     GraspObjects::GraspObjects(ros::NodeHandle nh):
     nodeHandle_(nh){
-            ROS_INFO("[Graspobjects] Node started.");
+        ROS_INFO("[GraspObjects] Node started.");
+        init();
     }
 
     GraspObjects::~GraspObjects(){
+    }
+
+    void GraspObjects::init(){
+        ROS_INFO("[GraspObjects] init().");
+        std::string pointCloudTopicName;
+        nodeHandle_.param("grasp_objects/eps_angle/value", epsAnglePlaneSegmentation_, float(0.06));
+        nodeHandle_.param("grasp_objects/distance_threshold/value",distanceThresholdPlaneSegmentation_, float(0.04));
+        nodeHandle_.param("subscribers/point_cloud/topic", pointCloudTopicName, std::string("/xtion/depth_registered/points"));
+
+        pointCloudSubscriber_ = nodeHandle_.subscribe(pointCloudTopicName, 10, &GraspObjects::pointCloudCallback, this);
+        outPointCloudPublisher_ = nodeHandle_.advertise<sensor_msgs::PointCloud2>("/transformed_cloud",5);
+    }
+
+
+    void GraspObjects::pointCloudCallback(const sensor_msgs::PointCloud2ConstPtr &pointCloud_msg){
+        ROS_INFO("[GraspObjects] pointCloudCallback()");
+        sensor_msgs::PointCloud2 pcOut;
+
+        pcl_ros::transformPointCloud("/base_footprint", *pointCloud_msg, pcOut,listener_);
+        pcOut.header.frame_id = "/base_footprint";
+        outPointCloudPublisher_.publish(pcOut);
+
+
     }
 
 }
