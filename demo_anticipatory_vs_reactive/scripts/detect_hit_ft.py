00babca31e4897a72306e78418a929c54d385de0
elisabeth-ms <emenende@pa.uc3m.es>
2023-04-17 16:00:34 +0000

    add activate/deactivate service for detect_hit_ft.

--- demo_anticipatory_vs_reactive/scripts/detect_hit_ft.py
+++ demo_anticipatory_vs_reactive/scripts/detect_hit_ft.py
@@ -8,6 +8,7 @@ from geometry_msgs.msg import WrenchStamped
 from std_msgs.msg import Float64
 import actionlib
 from pal_common_msgs.msg import EmptyAction, EmptyActionGoal
+from companion_msgs.srv import ActivateSupercuadricsComputation, ActivateSupercuadricsComputationResponse 
 
 class DetectHitFT(object):
     def __init__(self):
@@ -18,32 +19,44 @@ class DetectHitFT(object):
         self.pub_force_right = rospy.Publisher('force_wrist_right', Float64, queue_size=10)
         self.subscriber_wrist_left_ft = rospy.Subscriber("/wrist_left_ft/corrected", WrenchStamped, self.wristLeftFTCallback,  queue_size = 5)
         self.subscriber_wrist_right_ft = rospy.Subscriber("/wrist_right_ft/corrected", WrenchStamped, self.wristRightFTCallback,  queue_size = 5)
-
+        self.clientActivate = rospy.Service('/detect_hit_ft/activate', ActivateSupercuadricsComputation, self.handle_activate_detect_hit_ft)
         self.client = actionlib.SimpleActionClient('/gravity_compensation', EmptyAction)
+        self.activate = True
         self.client.wait_for_server()
+        rospy.loginfo("detect_hit_ft_node initialized.")
+
 
     def wristLeftFTCallback(self, ros_data):
-        data = ros_data.wrench
-        force = np.sqrt(data.force.x*data.force.x+data.force.y+data.force.y+data.force.z*data.force.z)
-        self.pub_force_left.publish(force)
-        if force>self.max_force or abs(data.force.x)>self.max_force or abs(data.force.y)>self.max_force or abs(data.force.z)>self.max_force:
-            goal = EmptyActionGoal()
-            self.client.send_goal(goal)
-            self.client.wait_for_result()
-            print(self.client.get_result())
-        print("Wrist left force(x: ", data.force.x, ", y: ", data.force.y, ", z: ", data.force.z, ")")
+        if self.activate:
+            data = ros_data.wrench
+            force = np.sqrt(data.force.x*data.force.x+data.force.y+data.force.y+data.force.z*data.force.z)
+            self.pub_force_left.publish(force)
+            if force>self.max_force or abs(data.force.x)>self.max_force or abs(data.force.y)>self.max_force or abs(data.force.z)>self.max_force:
+                goal = EmptyActionGoal()
+                self.client.send_goal(goal)
+                print("Gravity compensation activated!!")
+                self.client.wait_for_result()
+                print(self.client.get_result())
+            # print("Wrist left force(x: ", data.force.x, ", y: ", data.force.y, ", z: ", data.force.z, ")")
 
     def wristRightFTCallback(self, ros_data):
-        data = ros_data.wrench
-        force = np.sqrt(data.force.x*data.force.x+data.force.y+data.force.y+data.force.z*data.force.z)
-        self.pub_force_right.publish(force)
-        if force>self.max_force or abs(data.force.x)>self.max_force or abs(data.force.y)>self.max_force or abs(data.force.z)>self.max_force:
-            goal = EmptyActionGoal()
-            self.client.send_goal(goal)
-            self.client.wait_for_result()
-            print(self.client.get_result())
-        print("Wrist rightt force(x: ", data.force.x, ", y: ", data.force.y, ", z: ", data.force.z, ")")
+        if self.activate:
+            data = ros_data.wrench
+            force = np.sqrt(data.force.x*data.force.x+data.force.y+data.force.y+data.force.z*data.force.z)
+            self.pub_force_right.publish(force)
+            if force>self.max_force or abs(data.force.x)>self.max_force or abs(data.force.y)>self.max_force or abs(data.force.z)>self.max_force:
+                goal = EmptyActionGoal()
+                self.client.send_goal(goal)
+                print("Gravity compensation activated!!")
+                self.client.wait_for_result()
+                print(self.client.get_result())
+            # print("Wrist rightt force(x: ", data.force.x, ", y: ", data.force.y, ", z: ", data.force.z, ")")
 
+
+    def handle_activate_detect_hit_ft(self, req):
+        self.activate = req.activate
+        print("detect_hit_ft activate: ", self.activate)
+        return ActivateSupercuadricsComputationResponse(req.activate)
 
 
 if __name__ == '__main__':
